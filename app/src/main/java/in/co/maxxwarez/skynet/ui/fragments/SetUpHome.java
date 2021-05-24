71f00c8e660a0e0fe291177ab562eeb36a40abbe
abhinavnishant <abhinav.nishant@gmail.com>
2021-05-24 20:31:50 +0530

    Create user in DB at Login.java on login

---
+++ app/src/main/java/in/co/maxxwarez/skynet/ui/fragments/SetUpHome.java
@@ -0,0 +1,59 @@
+package in.co.maxxwarez.skynet.ui.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import in.co.maxxwarez.skynet.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SetUpHome#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SetUpHome extends Fragment implements View.OnClickListener {
+    private static final String TAG = "SkyNet";
+    public SetUpHome () {
+        // Required empty public constructor
+    }
+
+
+    public static SetUpHome newInstance () {
+        SetUpHome fragment = new SetUpHome();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView (LayoutInflater inflater, ViewGroup container,
+                              Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_set_up_home, container, false);
+        Button b = view.findViewById(R.id.setHome);
+        b.setOnClickListener(this);
+        return view;
+    }
+
+
+    @Override
+    public void onClick (View v) {
+        SetHomeDetail setHomeDetail = SetHomeDetail.newInstance();
+        FragmentManager fragmentManager = getParentFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left, R.anim.enter_from_left, R.anim.exit_to_right);
+        fragmentTransaction.replace(R.id.detailsList, setHomeDetail).addToBackStack(null).commit();
+
+    }
+}
